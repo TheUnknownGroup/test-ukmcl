f183629f172c6073470ec5c65f5512d2d1d70c7b
devonk15 <94760159+devonk15@users.noreply.github.com>
2026-08-22 18:56:08 -0400

    uhm [re]

--- sh/move.sh
+++
@@ -1,7 +0,0 @@
-cp finals/appimage/*.AppImage final/
-cp finals/deb/*.deb final/
-cp finals/rpm/*.rpm final/
-cp finals/dmg/*.dmg final/
-cp finals/macos/*.tar.gz final/
-cp finals/msi/*.msi final/
-cp finals/nsis/*.exe final/
